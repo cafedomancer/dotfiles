980d944a2c7e7badd94662e640f0ec58a4222041
cafedomancer <cafedomancer@gmail.com>
2016-01-16 01:02:26 +0900

    Update config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,16 @@
+abbr -a ga git add
+abbr -a gb git branch
+abbr -a gc git commit
+abbr -a gco git checkout
+abbr -a gd git diff
+abbr -a gl git pull
+abbr -a glg git log
+abbr -a gp git push
+abbr -a gst git status
+
+abbr -a grep grep --color=auto
+
+abbr -a ls ls -FG
+abbr -a la ls -AFG
+
+abbr -a tree tree -CF
