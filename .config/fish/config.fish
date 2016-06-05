a52fd6dce78488e5ad94de01a5c922f851829067
cafedomancer <cafedomancer@gmail.com>
2016-06-05 21:11:32 +0900

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,6 @@
-abbr -a be bundle exec
-
 abbr -a ga git add
 abbr -a gb git branch
-abbr -a gc git commit -v
+abbr -a gc git commit
 abbr -a gco git checkout
 abbr -a gd git diff
 abbr -a gl git pull
@@ -10,9 +8,11 @@ abbr -a glg git log
 abbr -a gp git push
 abbr -a gst git status
 
-abbr -a grep grep --color=auto
+alias grep "grep --color=auto"
+alias la "ls -AFG"
+alias ls "ls -FG"
+alias tree "tree -CF"
 
-abbr -a ls ls -FG
-abbr -a la ls -AFG
-
-abbr -a tree tree -CF
+eval (hub alias -s)
+status --is-interactive; and . (nodenv init -|psub)
+status --is-interactive; and . (rbenv init -|psub)
