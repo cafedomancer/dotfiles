7fceaf820e11ee838a31e92367976b2494b9270b
Shohei Umemoto <cafedomancer@gmail.com>
2016-11-09 17:38:07 +0900

    Add peco_select_repo

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -17,3 +17,25 @@ alias tree 'tree -CF'
 eval (hub alias -s)
 status --is-interactive; and . (nodenv init -|psub)
 status --is-interactive; and . (rbenv init -|psub)
+
+# rearrange the following configurations later
+
+function peco_select_repo
+  if test (count $argv) = 0
+    set peco_flags --layout=top-down
+  else
+    set peco_flags --layout=top-down --query "$argv"
+  end
+
+  ghq list --full-path | peco $peco_flags | read repo
+
+  if [ $repo ]
+    cd $repo
+  else
+    cd ''
+  end
+end
+
+function fish_user_key_bindings
+  bind \cr 'peco_select_repo'
+end
