22ced5ada0b9cd0aeaa58fdd4db2036c9201a613
Shohei Umemoto <cafedomancer@gmail.com>
2017-02-02 23:39:36 +0900

    Print the number of background jobs

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,16 +1,22 @@
+function __fish_jobs_prompt --description "Print the number of background jobs"
+    set -l jobs (jobs | wc -l | tr -d '[:space:]')
+    printf "(%s)" $jobs
+end
+
 function fish_prompt --description 'Write out the prompt'
-	# Just calculate these once, to save a few cycles when displaying the prompt
-   if not set -q __fish_prompt_hostname
-       set -g __fish_prompt_hostname (hostname -s)
-   end
+    # Just calculate these once, to save a few cycles when displaying the prompt
+    if not set -q __fish_prompt_hostname
+        set -g __fish_prompt_hostname (hostname -s)
+    end
 
-   if not set -q __fish_prompt_normal
-       set -g __fish_prompt_normal (set_color normal)
-   end
+    if not set -q __fish_prompt_normal
+        set -g __fish_prompt_normal (set_color normal)
+    end
 
-   if not set -q __fish_prompt_cwd
-       set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-   end
+    if not set -q __fish_prompt_cwd
+        set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+    end
 
-   echo -n -s "$USER" @ "$__fish_prompt_hostname" ' ' "$__fish_prompt_cwd" (prompt_pwd) (__fish_vcs_prompt) "$__fish_prompt_normal" '> '
+
+    echo -n -s "$__fish_prompt_cwd" (__fish_jobs_prompt) "$__fish_prompt_normal" ' ' "$USER" @ "$__fish_prompt_hostname" ' ' "$__fish_prompt_cwd" (prompt_pwd) (__fish_vcs_prompt) "$__fish_prompt_normal" '> '
 end
