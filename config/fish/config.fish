0cd5226b368d1de0578770b2b7f1b9bd539283ef
Shohei Umemoto <cafedomancer@gmail.com>
2019-06-22 22:03:28 +0900

    Update

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,1 +1,68 @@
 source ~/.asdf/asdf.fish
+
+if status --is-interactive
+    set -g fish_user_abbreviations
+
+    abbr -a ls 'exa'
+    abbr -a ll 'exa -l'
+    abbr -a la 'exa -al'
+    abbr -a tree 'exa -T'
+    abbr -a find 'fd'
+    abbr -a curl 'http'
+    abbr -a grep 'command rg'
+
+    abbr -a vim 'nvim'
+    abbr -a ag 'command rg'
+
+    abbr -a dco 'docker-compose'
+    abbr -a dcb 'docker-compose build'
+    abbr -a dce 'docker-compose exec'
+    abbr -a dcps 'docker-compose ps'
+    abbr -a dcrestart 'docker-compose restart'
+    abbr -a dcrm 'docker-compose rm'
+    abbr -a dcr 'docker-compose run'
+    abbr -a dcstop 'docker-compose stop'
+    abbr -a dcup 'docker-compose up'
+    abbr -a dcupd 'docker-compose up -d'
+    abbr -a dcdn 'docker-compose down'
+    abbr -a dcl 'docker-compose logs'
+    abbr -a dclf 'docker-compose logs -f'
+    abbr -a dcpull 'docker-compose pull'
+    abbr -a dcstart 'docker-compose start'
+
+    abbr -a ga 'git add'
+    abbr -a gb 'git branch'
+    abbr -a gc 'git commit'
+    abbr -a gco 'git checkout'
+    abbr -a gd 'git diff'
+    abbr -a gl 'git pull'
+    abbr -a glg 'git log'
+    abbr -a gp 'git push'
+    abbr -a grb 'git rebase'
+    abbr -a gsh 'git show'
+    abbr -a gst 'git status'
+
+    abbr -a be 'bundle exec'
+    abbr -a bi 'bundle install'
+    abbr -a bu 'bundle update'
+
+    abbr -a rc 'bin/rails console'
+    abbr -a rd 'bin/rails destroy'
+    abbr -a rdb 'bin/rails dbconsole'
+    abbr -a rg 'bin/rails generate'
+    abbr -a rgm 'bin/rails generate migration'
+    abbr -a rp 'bin/rails plugin'
+    abbr -a ru 'bin/rails runner'
+    abbr -a rs 'bin/rails server'
+    abbr -a rsd 'bin/rails server --debugger'
+    abbr -a rsp 'bin/rails server --port'
+
+    abbr -a rdm 'bin/rake db:migrate'
+    abbr -a rdr 'bin/rake db:rollback'
+    abbr -a rdc 'bin/rake db:create'
+    abbr -a rds 'bin/rake db:seed'
+    abbr -a rdd 'bin/rake db:drop'
+    abbr -a rr 'bin/rake routes'
+end
+
+set -g fish_user_paths "/Users/business/.go/bin" $fish_user_paths
--- config/nvim/init.vim
+++ config/nvim/init.vim
@@ -1,7 +1,59 @@
 call plug#begin('~/.local/share/nvim/plugged')
 
+Plug '/usr/local/opt/fzf'
+Plug '907th/vim-auto-save'
+Plug 'Yggdroot/indentLine'
 Plug 'chriskempson/base16-vim'
+Plug 'ervandew/supertab'
+Plug 'junegunn/fzf.vim'
+Plug 'junegunn/vim-easy-align'
+Plug 'scrooloose/nerdcommenter'
+Plug 'scrooloose/nerdtree'
+Plug 'tpope/vim-eunuch'
+Plug 'tpope/vim-fugitive'
+Plug 'tpope/vim-rails'
+Plug 'tpope/vim-sensible'
+Plug 'tpope/vim-sleuth'
 
 call plug#end()
 
 colorscheme base16-default-dark
+
+let mapleader = ' '
+let maplocalleader = ' '
+
+set cursorline
+set lazyredraw
+set list
+set splitbelow
+set splitright
+
+set nobackup
+set noswapfile
+set noundofile
+set nowritebackup
+
+noremap n nzz
+noremap N Nzz
+
+" 907th/vim-auto-save
+let g:auto_save = 1
+
+" Yggdroot/indentLine
+let g:indentLine_color_term = 10
+let g:indentLine_char = '│'
+
+" junegunn/fzf.vim
+noremap <C-P> :Files<CR>
+
+" junegunn/vim-easy-align
+xmap ga <Plug>(EasyAlign)
+nmap ga <Plug>(EasyAlign)
+
+" scrooloose/nerdcommenter
+let g:NERDSpaceDelims = 1
+let g:NERDDefaultAlign = 'left'
+map <C-_> <Leader>c<Space>
+
+" scrooloose/nerdtree
+noremap <C-N> :NERDTreeToggle<CR>
