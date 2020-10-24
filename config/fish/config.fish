6fb81e501e99a85e9c3ebfb85978b29c56ef9866
Shohei Umemoto <cafedomancer@gmail.com>
2020-10-24 18:05:02 +0900

    Update config.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,12 +1,1 @@
 source (brew --prefix asdf)/asdf.fish
-
-if status --is-interactive
-    set BASE16_SHELL "$HOME/.config/base16-shell/"
-    source "$BASE16_SHELL/profile_helper.fish"
-end
-
-if status --is-interactive
-    alias ls 'exa'
-    alias ll 'exa --long'
-    alias la 'exa --all --long'
-end
