aa5fd2eb266533399bf554ddc2a654a764f95923
Shohei Umemoto <cafedomancer@gmail.com>
2022-03-09 10:24:18 +0900

    Update

--- config/fish/functions/git_current_branch.fish
+++
@@ -1,9 +0,0 @@
-function git_current_branch
-    set ref (command git symbolic-ref --quiet HEAD 2> /dev/null)
-    set ret $status
-    if test $ret -ne 0
-        test $ret -eq 128 && return
-        set ref (command git rev-parse --short HEAD 2> /dev/null) || return
-    end
-    echo (string trim --chars=refs/heads/ $ref)
-end
